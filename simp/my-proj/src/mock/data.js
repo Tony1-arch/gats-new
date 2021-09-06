22631513a5f0dfc53713868d2bef3f942907dc4a
Tony1-arch <67202712+Tony1-arch@users.noreply.github.com>
2021-09-06 12:53:07 +0400

    edited web

---
+++ simp/my-proj/src/mock/data.js
@@ -0,0 +1,97 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: ' I am ronald senyonjo', // e.g: 'Name | Developer'
+  lang: 'eng', // e.g: en, es, fr, jp
+  description: 'i am ronald senyonjo', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: 'I am',
+  name: 'Ronald Senyonjo',
+  subtitle: ' African Corridor Incharge Instant Cash Global Money Transfer',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'ronie.jpg',
+  paragraphOne:
+    'Ronald Senyonjo is a Marketing Executive professional at Instant Cash Global Money Transfer [A memeber of Emirates Post Group] in the United Arab Emirates, An Inbound marketing and sales platform that helps companies and customers get attracted to the best money remittance',
+  paragraphTwo:
+    'Previously, Ronald worked as CRM inCharge for Bin Butti International Holding [Advanced Facilities Management] and after sales inCharge looking at customer satisfaction growth within the company. He graduated with honors from Makerere University with a Bachelor’s in social sciences',
+  paragraphThree: 'He has at some Point worked as Tv Video Journalist',
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'fb.jpg',
+    title: 'facebook',
+    info: 'All  i have are dreams. No one else would believe. No one else can see. No one else but me.',
+    info2:
+      'Well, its through understanding well your marketing prospects that will help you and your team to have a proper guild in lead generation which is an important topic for any business. I have found myself attracted to planning ,public relations,productive development,distribution,advertisment  ,community development and event organisation among others which are key aspects into marketing and goal attainment.',
+    url: 'https://www.facebook.com/senyonjo.ronnie', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'lin.jpg',
+    title: 'Linkedin',
+    info: 'Marketing Professional',
+    info2:
+      ' I always focus on helping our business build relationship with our customers through words , data and leads generating one of the most interesting aspects in my nature of work. Understanding the facts that today most businesses are heavily depending on lead generation as it enables the websites and social media platforms of such businesses to generate more traffic which may lead to an increase in conversion rates helping a business gain huge profits in the process.',
+    info3:
+      'Well, its through understanding well your marketing prospects that will help you and your team to have a proper guild in lead generation which is an important topic for any business. I have found myself attracted to planning ,public relations,productive development,distribution,advertisment,community development and event organisation among others which are key aspects into marketing and goal attainment.',
+
+    url: 'https://www.linkedin.com/in/ronald-s-44622299', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'ss.jpg',
+    title: 'Instagram',
+    info: '  Every person has their own journeys to success and happiness, but when human beings experience trauma or stressing life, you will find that it is not uncommon for their lives to unravel. Well, biggest passion is bringing hope to those that have been in such stressful experience by connecting them to what can bring them hope and joy. I know that no single approach is the right one for every individual who has been through such but have been trained in a range of modalities which can help others gain happiness. You find that my entire life besides my work schedule, I finish it with creating community and group gatherings in sports or events activities where we take time to share certain difficulties and help one another move a step ahead.My education background is a Bachelor’s degree in Social Sciences at Makerere University Kampala with a Major in Social Administration.',
+    info2: '',
+
+    url: 'https://www.instagram.com/senyonjo.ronnie', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'port.jpg',
+    title: 'Portrait Gallery 3R',
+    info: 'We are a photo editing company that provides editing services for professional wedding photography and videography, and that means we work with a lot of amazing pro wedding photographers. For years we have been following professional wedding photographers, portrait photographers, boudoir photographers, lifestyle photographers, and influencers for and compared the perfect of our work to the world market which has successfully been competitive for all our customers. “I don’t trust words. I trust pictures.”– Gilles Peress.',
+    info2: '',
+    url: 'https://www.facebook.com/portraitgalleryrandf0554243965media', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: 'Email Address',
+  btn: 'Yahoo',
+  email: 'senyonjor@yahoo.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'facebook',
+      url: 'https://www.facebook.com/senyonjo.ronnie', // if no repo, the button will not show up
+    },
+    {
+      id: nanoid(),
+      name: 'instagram',
+      url: 'https://www.instagram.com/senyonjo.ronnie',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/ronald-s-44622299', // if no repo, the button will not show up
+    },
+  ],
+};
+
+// Github start/fork buttons
